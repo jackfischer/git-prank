2825dec2b29b8861a488c4e4f90cdaf3ee7b9781
Jack Fischer <jfische8@binghamton.edu>
2016-02-03 20:58:19 -0500

    refactoring

--- git-prank.py
+++ git-prank.py
@@ -3,60 +3,30 @@ import git.exc as GitExceptions
 import os, signal, sys, time
 import datetime
 from isoweek import Week
-import itertools
 
 
 name = "Example Name"
 email = "email@example.com"
 commit_message = "Wow such git"
-
-
-def manager():
-  check_environment()
-  pid = os.fork()
-  if pid == 0: #child, displays spinner
-    show_spinner()
-  if pid != 0: #parent, do git work
-    write_commits()
-    os.kill(pid, signal.SIGKILL)
-    print "You're good!"
-
-    
-def check_environment():
-  try:
-      Repo(".")
-  except GitExceptions.InvalidGitRepositoryError:
-      print "Error: this isn't a git repo"
-      sys.exit(1)
-  try:
-    open("readme.md")
-  except:
-    print "no file (readme.md) to work with"
-    sys.exit(1)
-  return
-
-def write_commits():
-  current_week_num = datetime.date.isocalendar(datetime.date.today())[1]
-  current_year_num = datetime.date.today().year
-
-  make_year()
-  for _ in range(10):
-    make_p(current_year_num, current_week_num - 41) #left p
-    make_p(current_year_num, current_week_num - 14) #right p
-    make_o(current_year_num, current_week_num - 32) #left o
-    make_o(current_year_num, current_week_num - 23) #right o
+file_to_commit = "readme.md"
 
 
 def show_spinner():
   sys.stdout.write("Writing commits. This could take a bit... ")
-  spinner = itertools.cycle(['- ', '/ ', '| ', '\\ '])
+  spinner = generator()
   while True:
     sys.stdout.write(spinner.next())
     sys.stdout.flush()
     time.sleep(0.05)
     sys.stdout.write('\b\b')
 
-
+def generator():
+  i = -1
+  symbols = ['- ', '/ ', '| ', '\\ ']
+  while True:
+    i += 1
+    yield symbols[i % len(symbols)]
+ 
 def make_year():
   #today = datetime.datetime.today()
   today = make_today_object()
@@ -73,7 +43,6 @@ def make_today_object():
   today = datetime.date(y,m,d)
   return today
 
-
 def make_o(current_year_num, begin_week_num):
   current_week = Week(current_year_num, begin_week_num)
   commit(current_week.wednesday())
@@ -87,7 +56,6 @@ def make_o(current_year_num, begin_week_num):
   current_week = Week(current_year_num, begin_week_num + 3 )
   commit(current_week.wednesday())
   commit(current_week.thursday())
-
 
 def make_p(current_year_num, begin_week_num):
   current_week = Week(current_year_num, begin_week_num)
@@ -107,10 +75,8 @@ def make_p(current_year_num, begin_week_num):
   commit(current_week.tuesday())
   commit(current_week.wednesday())
 
-
-
 def commit(dt_obj):
-  new_file_path = "readme.md"
+  new_file_path = file_to_commit
 
   action_date = dt_obj.isoformat()
   action_date += " 12:00:00"
@@ -125,7 +91,36 @@ def commit(dt_obj):
   repo.index.commit(message, author=actor, committer=actor)
 
 
-if __name__ == "__main__":
-  manager()
+
+#Check environment
+try:
+  Repo(".")
+except GitExceptions.InvalidGitRepositoryError:
+  print "Error: this isn't a git repo"
+  sys.exit(1)
+try:
+  open(file_to_commit)
+except:
+  print "no file %s to work with" % file_to_commit
+  sys.exit(1)
+
+
+pid = os.fork()
+
+if pid == 0: #child, displays spinner
+  show_spinner()
+
+if pid != 0: #parent, do git work
+  current_week_num = datetime.date.isocalendar(datetime.date.today())[1]
+  current_year_num = datetime.date.today().year
+  make_year()
+  for _ in range(10):
+    make_p(current_year_num, current_week_num - 41) #left p
+    make_p(current_year_num, current_week_num - 14) #right p
+    make_o(current_year_num, current_week_num - 32) #left o
+    make_o(current_year_num, current_week_num - 23) #right o
+
+  os.kill(pid, signal.SIGKILL)
+  print "You're good!"
 
 
--- readme.md
+++ readme.md
@@ -1,5 +1,6 @@
 ##Little git practical joke
 
+* pip install gitpython
 * edit the top of the script with your email address of choice 
 * create a github repo and add the email's owner as a contibuter
 * run the script in a local git repository containing a "readme.md" (or any other file, and change the script) 
